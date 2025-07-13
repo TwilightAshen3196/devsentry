8f9446470a2e1f9355a1c1a7acaec0fea3865e93
TwilightAshen3196 <ayesha.i.04@outlook.com>
2025-07-13 19:44:50 +0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Acitvate virtual python environment (change by your path for environemnt)
+. .../venv/bin/activate
+
+# install requirements to environment
+pip3 install -r requirements
+
 # Start FastAPI backend
 uvicorn app.main:app --host 127.0.0.1 --port 8000 &
 
