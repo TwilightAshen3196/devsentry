b1d30ad1e051cc8206c9c631f646f481ef31e418
TwilightAshen3196 <ayesha.i.04@outlook.com>
2025-07-13 22:26:49 +0200

    edited ports

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@
 . ../venv/bin/activate
 
 # Start FastAPI backend
-uvicorn app.main:app --host 127.0.0.1 --port 8005 &
+uvicorn app.main:app --host 127.0.0.1 --port 8000 &
 
 # Start Streamlit frontend
-PYTHONPATH=. streamlit run ui/dashboard.py --server.port 8505
+streamlit run ui/dashboard.py --server.port 8501
